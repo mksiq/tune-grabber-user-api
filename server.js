de240dd386411d3db5656e7ece2591b00d0bf71c
mksiq <maickelsiqueira@gmail.com>
2021-04-01 13:24:21 -0400

    added get put delete routes to favorite tracks

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
 const passportJWT = require("passport-jwt");
-const dotenv = require("dotenv");
 
+const dotenv = require("dotenv");
 dotenv.config({ path: "./config/.env" });
+
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+const strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
+  console.log("inside middleware");
+  console.log("payload received", jwt_payload);
+
+  if (jwt_payload) {
+    // The following will ensure that all routes using
+    // passport.authenticate have a req.user._id, req.user.userName values
+    // that matches the request payload data
+    next(null, { _id: jwt_payload._id, userName: jwt_payload.userName });
+  } else {
+    next(null, false);
+  }
+});
+
+passport.use(strategy);
 
 const userService = require("./user-service.js");
 
@@ -15,6 +40,7 @@ const HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
+app.use(passport.initialize());
 
 /* TODO Add Routes Here */
 
@@ -41,9 +67,57 @@ app.post("/api/register", async (req, res) => {
 
 app.post("/api/login", async (req, res) => {
   try {
-    await userService.checkUser(req.body);
-    res.json({ message: "login successful" });
+    const data = await userService.checkUser(req.body);
+    const user = (({ _id, userName } = data), { _id, userName }); // lambda with destructuring
+    const token = jwt.sign(user, jwtOptions.secretOrKey);
+    console.log(token);
+    res.json({ message: "login successful", token: token });
   } catch (err) {
     res.status(422).json({ message: err });
   }
 });
+
+app.get(
+  "/api/user/favourites",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const data = await userService.getFavourites(req.user._id);
+      res.json(data);
+    } catch (err) {
+      console.log(err);
+      res.status(422).json({ message: err });
+    }
+  }
+);
+
+app.put(
+  "/api/user/favourites/:id",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const data = await userService.addFavourite(req.user._id, req.params.id);
+      res.json(data);
+    } catch (err) {
+      console.log(err);
+      res.status(422).json({ message: err });
+    }
+  }
+);
+
+app.delete(
+  "/api/user/favourites/:id",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const data = await userService.removeFavourite(
+        req.user._id,
+        req.params.id
+      );
+      res.json(data);
+    } catch (err) {
+      console.log(err);
+      res.status(422).json({ message: err });
+    }
+  }
+);
